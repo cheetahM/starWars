e4b691586fb98109243191cafe09e3ca74627344
Chetan Meshram <chmeshra@visa.com>
2017-08-21 23:32:24 -0700

    adding two servers and url module

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 // Here we require/import the HTTP module
 var http = require("http");
-// Here we define a port to listen to
-var PORT = 8080;
-// Here we create a generic function to handle requests and responses
-function handleRequest(request, response) {
+
+// ===========================================================================
+
+// Here we define ports we want to listen to
+var PORTONE = 7000;
+var PORTTWO = 7500;
+
+//===================================================================
+
+// Here we create two generic functions to handle requests and responses
+function handleRequestOne(request, response) {
   // The below statement is triggered (client-side) when the user visits the PORT URL
+  //response.end("You're a JavaScript mastermind!");
   response.end("It Works!! Path Hit: " + request.url);
 }
-// Here we use the Node HTTP package to create our server.
+
+function handleRequestTwo(request, response) {
+    response.end("You smell.");
+}
+
+// =======================================================================
+// Here we use the Node HTTP package to create our two servers.
 // We then pass it the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+var serverOne = http.createServer(handleRequestOne);
+var serverTwo = http.createServer(handleRequestTwo);
 // Here we start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
+serverOne.listen(PORTONE, function() {
   // The below statement is triggered (server-side) when a user visits the PORT URL
-  console.log("Server listening on: http://localhost:%s", PORT);
+  console.log("Server listening on: http://localhost:%s", PORTONE);
+});
+
+serverTwo.listen(PORTTWO, function() {
+    console.log("Server listening on: http://localhost:%s", PORTTWO);
 });
